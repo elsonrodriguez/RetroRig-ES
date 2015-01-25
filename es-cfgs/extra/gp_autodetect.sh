6832ca5c54cdc5674d087e43fe9329fd52501271
ProfessorKaos64 <mdeguzis@gmail.com>
2015-01-25 12:54:23 -0500

    changed install routine for scummvm

--- es-cfgs/extra/gp_autodetect.sh
+++ es-cfgs/extra/gp_autodetect.sh
@@ -2,8 +2,8 @@
 
 # Project RetroRig: https://github.com/ProfessorKaos64/RetroRig
 #
-# This script detects a change of the amount of available PS3 controller. 
-# If such a change was detected, it notifies XBMC.
+# This script detects a change of the amount of available GP controller. 
+# If such a change was detected, it notifies EmulationStation.
 #
 # If enabled, it will start RetroRig automatically 
 # for the first game controller being switched on.
@@ -11,25 +11,25 @@
 # Author: Jens-Christian, aka  beaumanvienna/JC
 # Author: Michael DeGuzis, aka  PK
 #
-# Revision: 20141030, JC, support for PS3 / USB and auto start XBMC function
+# Revision: 20141030, JC, support for GP / USB and auto start EmulationStation function
 # Revision: 20141229, PK, this will need overhauled for EmulationStation if need be
 
-autostartES_PS3_USB=enabled
-autostartES_PS3_BT=enabled
+autostartES_GP_USB=enabled
+autostartES_GP_BT=enabled
 
-PS3_BT_controllerAvailable=`cat /proc/bus/input/devices | grep "PLAYSTATION(R)3 Controller ("`
-PS3_USB_controllerAvailable=`lsusb | grep "PlayStation 3 Controller"`
+GP_BT_controllerAvailable=`cat /proc/bus/input/devices | egrep -w "PLAYSTATION(R)3 Controller|Xbox"`
+GP_USB_controllerAvailable=`lsusb | egrep -w "PlayStation 3 Controller|Xbox"`
 
 while [ 0 ]
 do
-  oldPS3_BT_controllerAvailable=$PS3_BT_controllerAvailable
-  oldPS3_USB_controllerAvailable=$PS3_USB_controllerAvailable  
+  oldGP_BT_controllerAvailable=$GP_BT_controllerAvailable
+  oldGP_USB_controllerAvailable=$GP_USB_controllerAvailable  
   
-  #check for PS3 / USB controller
+  #check for GP / USB controller
   
-  PS3_USB_controllerAvailable=`lsusb | grep "PlayStation 3 Controller"`
-  if [ "$PS3_USB_controllerAvailable" != "$oldPS3_USB_controllerAvailable" ]; then
-    echo "[begin:] PS3 / USB controller changed"
+  GP_USB_controllerAvailable=`lsusb | egrep -w "PlayStation 3 Controller|Xbox"`
+  if [ "$GP_USB_controllerAvailable" != "$oldGP_USB_controllerAvailable" ]; then
+    echo "[begin:] GP / USB controller changed"
     
     #first attempt
     service xboxdrv stop
@@ -41,8 +41,8 @@ do
     killall -9 xboxdrv
     service xboxdrv start
     
-    #auto start xbmc for first controller being switched on
-    if [ "$autostartES_PS3_USB" == "enabled" ]; then
+    #auto start EmulationStation for first controller being switched on
+    if [ "$autostartES_GP_USB" == "enabled" ]; then
       echo "auto start function"
       autostarted=false
       
@@ -50,17 +50,17 @@ do
         #retrorig-setup-es is not running
         echo "retrorig-setup-es is not running"
         
-        if [ -z "$oldPS3_USB_controllerAvailable" ]; then
+        if [ -z "$oldGP_USB_controllerAvailable" ]; then
           #first controller was switched on
           echo "first controller was switched on"
           
-          es_running=`ps ax |grep emulationstation |grep -v grep`
+          es_running=`ps ax |egrep -w emulationstation |egrep -wv grep`
           if [ -z "$es_running" ]; then
             # ES not running yet
             echo "EmulationStation not running yet"
         
             # get user name
-            user=`ps aux |grep "/bin/dbus-daemon --config-file"|grep -v grep|cut -f 1 -d " "`
+            user=`ps aux |grep "/bin/dbus-daemon --config-file"|egrep -wv grep|cut -f 1 -d " "`
         
             export export DISPLAY=:0.0
             echo "auto starting RetroRig-ES for user $user"
@@ -68,10 +68,10 @@ do
             autostarted=true
             
           else
-            echo "RetroRig-ES *not* started automatically: EmulationStation already running, xbmc_running="$es_running
+            echo "RetroRig-ES *not* started automatically: EmulationStation already running, EmulationStation_running="$es_running
           fi
         else
-          echo "RetroRig-ES *not* started automatically: first controller not switched on, oldPS3_USB_controllerAvailable="$oldPS3_USB_controllerAvailable
+          echo "RetroRig-ES *not* started automatically: first controller not switched on, oldGP_USB_controllerAvailable="$oldGP_USB_controllerAvailable
         fi
       else
         echo "RetroRig-ES *not* started automatically: setup_running="$setup_running
@@ -80,7 +80,7 @@ do
       #diagnostic message
       sleep 2
       if [ "$autostarted" == "true" ]; then
-        if [ -n "$(ps ax |grep emulationstation | grep -v grep)" ]; then
+        if [ -n "$(ps ax |grep emulationstation | egrep -wv grep)" ]; then
           echo "RetroRig-ES sucessfully started"
         else
           echo "attempt to start RetroRig-ES failed"
@@ -91,50 +91,50 @@ do
       echo "auto start function disabled"
     fi
     
-    #Avoid starting XBMC after USB is unplugged.
+    #Avoid starting EmulationStation after USB is unplugged.
     #For some reasons the game controller connects via
     #BT after being unplugged from USB
-    autostartES_PS3_BT=enabled
-    echo "[end:] PS3 / USB controller changed"
+    autostartES_GP_BT=enabled
+    echo "[end:] GP / USB controller changed"
   fi
   
-  #check for PS3 / Bluetooth controller
+  #check for GP / Bluetooth controller
   
-  PS3_BT_controllerAvailable=`cat /proc/bus/input/devices | grep "PLAYSTATION(R)3 Controller ("`
-  if [ "$PS3_BT_controllerAvailable" != "$oldPS3_BT_controllerAvailable" ]; then
-    echo "[begin:] PS3 / Bluetooth controller changed"
+  GP_BT_controllerAvailable=`cat /proc/bus/input/devices | egrep -w "PLAYSTATION(R)3 Controller|Xbox"`
+  if [ "$GP_BT_controllerAvailable" != "$oldGP_BT_controllerAvailable" ]; then
+    echo "[begin:] GP / Bluetooth controller changed"
   
-    #auto start xbmc for first controller being switched on
-    if [ "$autostartES_PS3_BT" == "enabled" ]; then
+    #auto start EmulationStation for first controller being switched on
+    if [ "$autostartES_GP_BT" == "enabled" ]; then
       echo "auto start function"
       autostarted=false
-      setup_running=`ps ax |grep retrorig-es-setup| grep -v grep`
+      setup_running=`ps ax |grep retrorig-es-setup| egrep -wv grep`
       
       if [ -z "$setup_running" ]; then
         #retrorig-setup is not running
         echo "retrorig-setup is not running"
         
-        if [ -z "$oldPS3_BT_controllerAvailable" ]; then
+        if [ -z "$oldGP_BT_controllerAvailable" ]; then
           #first controller was switched on
           echo "first controller was switched on"
       
-          es_running=`ps ax |grep emulationstation |grep -v grep`
+          es_running=`ps ax |grep emulationstation`
           if [ -z "$es_running" ]; then
-            # xbmc not running yet
+            # EmulationStation not running yet
             echo "EmulationStation not running yet"
         
             # get user name
-            user=`ps aux |grep "/bin/dbus-daemon --config-file"|grep -v grep|cut -f 1 -d " "`
+            user=`ps aux |grep "/bin/dbus-daemon --config-file"|egrep -wv |cut -f 1 -d " "`
         
             export export DISPLAY=:0.0
             echo "auto starting RetroRig-ES for user $user"
             sudo -u $user -g $user -s /usr/share/applications/startRetroES.sh &
             autostarted=true
           else
-            echo "RetroRig-ES *not* started automatically: XBMC already running, xbmc_running="$es_running
+            echo "RetroRig-ES *not* started automatically: EmulationStation already running, EmulationStation_running="$es_running
           fi
         else
-          echo "RetroRig-ES *not* started automatically: first controller not switched on, oldPS3_BT_controllerAvailable="$oldPS3_BT_controllerAvailable
+          echo "RetroRig-ES *not* started automatically: first controller not switched on, oldGP_BT_controllerAvailable="$oldGP_BT_controllerAvailable
         fi
       else
         echo "RetroRig-ES *not* started automatically: setup_running="$setup_running
@@ -143,7 +143,7 @@ do
       #diagnostic message
       sleep 2
       if [ "$autostarted" == "true" ]; then
-        if [ -n "$(ps ax | grep emulationstation |grep -v grep)" ]; then
+        if [ -n "$(ps ax | grep emulationstation |egrep -wv)" ]; then
           echo "RetroRig-ES sucessfully started"
         else
           echo "attempt to start RetroRig-ES failed"
@@ -154,7 +154,7 @@ do
       echo "auto start function disabled"
     fi
     
-    echo "[end:] PS3 / Bluetooth controller changed"
+    echo "[end:] GP / Bluetooth controller changed"
   fi
   sleep 1
 done
